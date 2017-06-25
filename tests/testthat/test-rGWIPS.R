3b071dd03a6871a7d193f0dd079e96fa6dc65c1d
Paweł Szczęsny <ps@pawelszczesny.org>
2017-06-25 22:44:38 +0200

    Added testing for GRanges class

--- tests/testthat/test-rGWIPS.R
+++ tests/testthat/test-rGWIPS.R
@@ -2,12 +2,20 @@
 
 context("Loading GWIPS data")
 
-test_that("loadGWIPSdata function fails on wrong file paths",{
+test_that("loadGWIPSdata function correctly loads data",{
 
   expect_error(loadGWIPSdata("wrongpath", system.file(package="rGWIPS", "extdata","Liu13_All.RiboProElongRev.bw")),
                "file.exists(forward) is not TRUE", fixed=TRUE)
 
   expect_error(loadGWIPSdata(system.file(package="rGWIPS", "extdata","Liu13_All.RiboProElongRev.bw"),"wrongpath"),
                "file.exists(reverse) is not TRUE", fixed=TRUE)
+
+  for_bw = system.file(package = "rGWIPS", "extdata", "Liu13_All.RiboProElongFor.bw")
+  rev_bw = system.file(package = "rGWIPS", "extdata", "Liu13_All.RiboProElongRev.bw")
+
+  loadGWIPSdata(forward = for_bw, reverse = rev_bw)
+
+  expect_is(gwips_forw, "GRanges")
+  expect_is(gwips_rev, "GRanges")
 
 })
